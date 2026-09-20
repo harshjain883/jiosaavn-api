b5db301778a530ab565a7e117696db04a275858e
harshjain883 <115611849+harshjain883@users.noreply.github.com>
2026-09-20 12:56:44 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
 import { handle } from '@hono/node-server/vercel'
-import { app } from '../src/app'
+import { App } from '../src/app'
 
-export default handle(app)
+const server = new App()
+
+export default handle(server.app)
